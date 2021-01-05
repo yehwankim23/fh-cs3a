8b69f4533ee0fe2d5c674932ffc333c8181a0299
yehwankim23 <yehwankim23@gmail.com>
2021-01-05 22:50:31 +0900

    Add assignment 1

---
+++ 20210108_Assignment_1/DemoGit.py
@@ -0,0 +1,14 @@
+docstring = """ Demo Git: Opening Lines
+    Submitted by Ye-Hwan Kim
+    Submitted: Jan 4, 2021
+    This program demonstrates printing lines of text to the screen
+"""
+
+
+def print_header():
+    print("Hello.")
+    print("My name is Ye-Hwan Kim.")
+
+
+if __name__ == '__main__':
+    print_header()
